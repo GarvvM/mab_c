f45b8d8dfaeadce80144a12e2cc74188d95f8b6f
BOI LAB <boilab@mycampusedu.net>
2024-05-31 10:40:58 +0530

    myommit

---
+++ Accounb.java
@@ -0,0 +1,32 @@
+package Gaurav;
+
+
+
+class Account
+{
+	String name="Vinayak";
+	int Account_no = 234;
+	float balance = 10000;
+	public void display()
+	{
+		System.out.println("Name:"+name+ "\n Account no.:"+Account_no+"\n balance");
+	}
+}
+class Loan_account extends Account
+{
+	int loan_amount=2000;
+	float available_balance = balance - loan_amount;
+	public void display()
+	{
+		System.out.println("Available balance: "+available_balance);
+	}
+}
+public class Accoubt {
+
+	public static void main(String[] args) {
+		Loan_account obj=new Loan_account();
+		obj.display();
+
+	}
+
+}
